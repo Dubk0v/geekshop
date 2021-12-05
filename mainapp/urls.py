8b6cfea85b93a3c7988f75fe2c52befa7366d019
Dubkov <yevgeniydubkov@gmail.com>
2021-12-06 02:26:21 +0300

    working

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
 
     path('', products, name='products'),
     path('detail/<int:pk>/', ProductDetail.as_view(), name='detail'),
+    path('category/<int:id_category>', products, name='category'),
+    path('page/<int:page>', products, name='page'),
 ]
